8f07d202157fea5753c1cd67f2cd33a62d39771c
Azamat <azamat-nazmutdinov@yandex.ru>
2023-02-23 07:20:09 +0500

    Java seminar 5

---
+++ seminar5/task2/Task2.java
@@ -0,0 +1,36 @@
+// 2. Пусть дан список сотрудников. Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+// Отсортировать по убыванию популярности.
+
+package seminar5.task2;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task2 {
+    private static final String[] workers = {"Иван Иванов", "Светлана Петрова", "Кристина Белова",
+                                            "Анна Мусина", "Анна Крутова", "Иван Юрин",
+                                            "Петр Лыков", "Павел Чернов", "Петр Чернышов",
+                                            "Мария Федорова", "Марина Светлова", "Мария Савина",
+                                            "Мария Рыкова", "Марина Лугова", "Анна Владимирова",
+                                            "Иван Мечников", "Петр Петин", "Иван Ежов"};
+
+    public static void main(String[] args) {
+        Map<String, Integer> namePopularity = new HashMap<>();
+        String name = "";
+
+        for (String worker: workers) {
+            name = worker.split(" ")[0];
+            if (namePopularity.containsKey(name)) {
+                namePopularity.put(name, namePopularity.get(name) + 1);
+            } else {
+                namePopularity.put(name, 1);
+            }
+        }
+
+        namePopularity.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(System.out::println);
+    }
+}
---
+++ seminar5/task3/Task3.java
@@ -0,0 +1,54 @@
+// 3*. Реализовать алгоритм пирамидальной сортировки (HeapSort).
+
+package seminar5.task3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Integer[] numbers = new Integer[20];
+        Random random = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void sort(Integer[] numbers) {
+        for (int i = numbers.length / 2 - 1; i >= 0; i--) {
+            updateHeap(numbers, numbers.length, i);
+        }
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int temp = numbers[0];
+            numbers[0] = numbers[i];
+            numbers[i] = temp;
+
+            updateHeap(numbers, i, 0);
+        }
+    }
+
+    private static void updateHeap(Integer[] numbers, int length, int i) {
+        int rootPos = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < length && numbers[leftChild] > numbers[rootPos]) {
+            rootPos = leftChild;
+        }
+        if (rightChild < length && numbers[rightChild] > numbers[rootPos]) {
+            rootPos = rightChild;
+        }
+        if (rootPos != i) {
+            int temp = numbers[i];
+            numbers[i] = numbers[rootPos];
+            numbers[rootPos] = temp;
+
+            updateHeap(numbers, length, rootPos);
+        }
+    }
+}
